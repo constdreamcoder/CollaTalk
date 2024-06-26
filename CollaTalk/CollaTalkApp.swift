aae944a8eab3b9e27301284d3a6f192089b18864
constdreamcoder <scdreamricher@gmail.com>
2024-06-26 15:40:30 +0900

    [CHORE] 프로젝트 생성

---
+++ CollaTalk/CollaTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollaTalkApp.swift
+//  CollaTalk
+//
+//  Created by SUCHAN CHANG on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct CollaTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
