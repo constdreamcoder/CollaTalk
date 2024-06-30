9ffa57ae0a1f62515fd0e15ec5dc5936c5ec880f
constdreamcoder <scdreamricher@gmail.com>
2024-06-30 22:18:39 +0900

    [ASSET] 공통 Typography 정의

---
+++ CollaTalk/CommonStyle.swift
@@ -0,0 +1,18 @@
+//
+//  CommonStyle.swift
+//  CollaTalk
+//
+//  Created by SUCHAN CHANG on 6/30/24.
+//
+
+import SwiftUI
+
+enum CommonStyle {
+    enum Typography {
+        static let title1: Font = .system(size: 22, weight: .bold)
+        static let title2: Font = .system(size: 14, weight: .bold)
+        static let bodyBold: Font = .system(size: 13, weight: .bold)
+        static let body: Font = .system(size: 13, weight: .regular)
+        static let caption: Font = .system(size: 12, weight: .regular)
+    }
+}
