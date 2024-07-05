27b7aaef20833b15b789ce7110ee032b07253fcf
constdreamcoder <scdreamricher@gmail.com>
2024-07-05 17:59:49 +0900

    [ASSET] 폰트 Asset Font Extension으로 그룹화 전환

---
+++ CollaTalk/Helpers/Extensions/Fonts.swift
@@ -0,0 +1,16 @@
+//
+//  Fonts.swift
+//  CollaTalk
+//
+//  Created by SUCHAN CHANG on 7/5/24.
+//
+
+import SwiftUI
+
+extension Font {
+    static let title1: Font = .system(size: 22, weight: .bold)
+    static let title2: Font = .system(size: 14, weight: .bold)
+    static let bodyBold: Font = .system(size: 13, weight: .bold)
+    static let body: Font = .system(size: 13, weight: .regular)
+    static let caption: Font = .system(size: 12, weight: .regular)
+}
