7aa52e70d445a4d88cc945b64e436a067b95ffdd
constdreamcoder <scdreamricher@gmail.com>
2024-07-22 17:07:48 +0900

    [FEAT] DM 빈 멤버 목록 화면에 팀원 초대 기능 연결

--- CollaTalk/Scenes/Main/DM/DMEmptyView.swift
+++ CollaTalk/Scenes/Main/DM/DMEmptyView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct DMEmptyView: View {
+    
+    @EnvironmentObject private var store: AppStore
+    
     var body: some View {
         VStack(spacing: 19) {
             Text("워크스페이스에\n멤버가 없어요.")
@@ -19,6 +22,14 @@ struct DMEmptyView: View {
             
             CustomButton {
                 print("팀원 초대하기")
+                
+                if store.state.user?.userId == store.state.workspaceState.selectedWorkspace?.ownerId {
+                    print("팀원 초대")
+                    store.dispatch(.navigationAction(.presentInviteMemeberView(present: true)))
+                } else {
+                    print("팀원 초대 불가")
+                    store.dispatch(.inviteMemeberAction(.showToastMessageForNoRightToInviteMember))
+                }
             } label: {
                 Text("팀원 초대하기")
             }
